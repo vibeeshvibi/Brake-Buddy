2eddb8b31c9dfe66c63d64747547d2dcd94eee9e
Vibeesh N <107397209+vibeeshvibi@users.noreply.github.com>
2024-07-21 22:43:33 +0530

    Update Home.jsx

--- Front-End/src/Components/Home.jsx
+++ Front-End/src/Components/Home.jsx
@@ -7,14 +7,12 @@ function Home() {
     navigate(`../login`);
   };
 
-
   return (
-    <div className="flex">
-      {/* Left Side Content */}
-      <div className="w-1/2 flex flex-col justify-center items-center p-8">
+    <div className="flex flex-col lg:flex-row min-h-screen">
+      <div className="w-full lg:w-1/2 flex flex-col justify-center items-center p-8">
         <div className="container max-w-md text-center">
-          <h1 className="text-7xl font-bold mb-4 leading-tight ">Looking for a Service!</h1>
-          <h3  className="text-5xl text-red-600 font-semibold mb-6 leading-tight">We are Here</h3>
+          <h1 className="text-4xl lg:text-7xl font-bold mb-4 leading-tight">Looking for a Service!</h1>
+          <h3 className="text-3xl lg:text-5xl text-red-600 font-semibold mb-6 leading-tight">We are Here</h3>
           <button
             className="bg-blue-800 hover:bg-blue-600 text-white py-3 px-6 rounded-lg shadow-lg"
             onClick={navigateLogin}
@@ -23,12 +21,8 @@ function Home() {
           </button>
         </div>
       </div>
-
-      {/* Right Side Image */}
-      <div className="w-1/2">
-        <div className="flex justify-center items-center h-screen">
-          <img src={Bike} alt="bike" className="max-h-full mr-10  " />
-        </div>
+      <div className="w-full lg:w-1/2 flex justify-center items-center p-4 lg:p-0">
+        <img src={Bike} alt="bike" className="w-full h-auto lg:max-h-full lg:mr-10" />
       </div>
     </div>
   );
