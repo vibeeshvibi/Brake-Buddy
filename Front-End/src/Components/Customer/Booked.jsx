6e7d91504565629616fd6f70866b8d0aeefd78d1
Vibeesh N <107397209+vibeeshvibi@users.noreply.github.com>
2024-07-21 22:59:32 +0530

    Update Booked.jsx

--- Front-End/src/Components/Customer/Booked.jsx
+++ Front-End/src/Components/Customer/Booked.jsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import useAuth from '../Auth';
 import SimpleBackdrop from '../Backdrop';
+
 function Booked() {
   useAuth();
   const navigate = useNavigate();
-  const [loading,setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
   const [data, setData] = useState([]);
-  console.log(data);
 
   useEffect(() => {
     const email = sessionStorage.getItem('Email');
@@ -31,53 +30,52 @@ function Booked() {
 
   const updateservice = (_id) => {
     sessionStorage.setItem('BookingID', _id);
-    navigate(`../viewbooking`);
+    navigate('../viewbooking');
   };
 
   return (
-    <>
-    <div className="min-h-screen  p-5">
-      <h1 className="text-4xl font-bold text-center  text-gray-800">Status</h1>
+    <div className="min-h-screen p-5 bg-gray-50">
+      <h1 className="text-4xl font-bold text-center text-gray-800 mb-10">Status</h1>
       <div className="overflow-x-auto">
-        <div className="min-w-full bg-white shadow-md rounded-lg overflow-hidden">
-          {!loading?
-
-          <table className="min-w-full bg-white mt-10">
-            <thead>
-              <tr className="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
-                <th className="py-3 px-6 text-left">Date</th>
-                <th className="py-3 px-6 text-left">Name</th>
-                <th className="py-3 px-6 text-left">Bike Number</th>
-                <th className="py-3 px-6 text-left">Service</th>
-                <th className="py-3 px-6 text-left">Status</th>
-                <th className="py-3 px-6 text-center">Action</th>
-              </tr>
-            </thead>
-            <tbody className="text-gray-600 text-md font-light">
-              {data.map((row) => (
-                <tr key={row._id} className="border-b border-gray-200 ">
-                  <td className="py-3 px-6 text-left whitespace-nowrap">{row.date}</td>
-                  <td className="py-3 px-6 text-left">{row.name}</td>
-                  <td className="py-3 px-6 text-left">{row.vno}</td>
-                  <td className="py-3 px-6 text-left">{row.service.join(', ')}</td>
-                  <td className="py-3 px-6 text-left">{row.status}</td>
-                  <td className="py-3 px-6 text-center">
-                    <button
-                      onClick={() => updateservice(row._id)}
-                      className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition duration-200"
-                    >
-                      View
-                    </button>
-                  </td>
+        <div className="min-w-full bg-white shadow-md rounded-lg overflow-wrap">
+          {!loading ? (
+            <table className="min-w-full bg-white">
+              <thead>
+                <tr className="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+                  <th className="py-3 px-6 text-left">Date</th>
+                  <th className="py-3 px-6 text-left">Name</th>
+                  <th className="py-3 px-6 text-left">Bike Number</th>
+                  <th className="py-3 px-6 text-left">Service</th>
+                  <th className="py-3 px-6 text-left">Status</th>
+                  <th className="py-3 px-6 text-center">Action</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>:<SimpleBackdrop />
-                    }
+              </thead>
+              <tbody className="text-gray-600 text-md font-light">
+                {data.map((row) => (
+                  <tr key={row._id} className="border-b border-gray-200">
+                    <td className="py-3 px-6 text-left whitespace-nowrap">{row.date}</td>
+                    <td className="py-3 px-6 text-left">{row.name}</td>
+                    <td className="py-3 px-6 text-left">{row.vno}</td>
+                    <td className="py-3 px-6 text-left">{row.service.join(', ')}</td>
+                    <td className="py-3 px-6 text-left">{row.status}</td>
+                    <td className="py-3 px-6 text-center">
+                      <button
+                        onClick={() => updateservice(row._id)}
+                        className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition duration-200"
+                      >
+                        View
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          ) : (
+            <SimpleBackdrop />
+          )}
         </div>
       </div>
     </div>
-    </>
   );
 }
 
