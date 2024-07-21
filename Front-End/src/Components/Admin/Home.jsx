449bd583c5c05c32503202d6b838d567404f5441
Vibeesh N <107397209+vibeeshvibi@users.noreply.github.com>
2024-07-21 23:18:35 +0530

    Update Home.jsx

--- Front-End/src/Components/Admin/Home.jsx
+++ Front-End/src/Components/Admin/Home.jsx
@@ -15,20 +15,20 @@ function Home() {
     <div className="min-h-screen flex flex-col md:flex-row p-8">
       <div className="AdminHomeDetails flex-1 md:mr-8 flex justify-center items-center md:items-start">
         <div className="container mx-auto text-center md:text-left px-3">
-          <h1 className="text-8xl font-bold mb-4 pt-20">Welcome Admin</h1>
-          <h3 className="text-4xl pt-5 text-red-700 font-bold mb-4">Here are the Bookings!</h3>
+          <h1 className="text-4xl md:text-8xl font-bold mb-4 pt-20">Welcome Admin</h1>
+          <h3 className="text-2xl md:text-4xl pt-5 text-red-700 font-bold mb-4">Here are the Bookings!</h3>
           <div className='pt-5'>
-          <button
-            className="bg-red-700  text-white py-2 px-4 rounded shadow hover:bg-red-300"
-            onClick={navigateBooking}
-          >
-            View Booking
-          </button>
+            <button
+              className="bg-red-700 text-white py-2 px-4 rounded shadow hover:bg-red-300"
+              onClick={navigateBooking}
+            >
+              View Booking
+            </button>
           </div>
         </div>
       </div>
-      <div className="AdminHomeImage flex-1 flex justify-center items-center">
-        <img src={BikeRepair} alt="Bike Repair" className="w-full h-auto " />
+      <div className="AdminHomeImage flex-1 flex justify-center items-center mt-8 md:mt-0">
+        <img src={BikeRepair} alt="Bike Repair" className="w-full h-auto max-w-sm md:max-w-full" />
       </div>
     </div>
   );
